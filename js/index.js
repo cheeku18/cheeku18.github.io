6559697c49c756d8181242da2f1979a04f2c1e13
Arun Gopal Govindaswamy <aarunkarthii@gmail.com>
2019-06-23 00:38:02 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,4 @@
 function toggleAnswer(f) {
 			var ans = "ans"+f.id.split('ques')[1];
- 			var x = document.getElementById(ans);
 			$("#"+ans).toggle();
-			if (x.style.display === "none") {
-				x.style.display = "block";
-			} else {
-				x.style.display = "none";
-			}
 }
