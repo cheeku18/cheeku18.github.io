2738a45e69191417fc9e929a72ecd4ed0873b9a7
Arun Gopal Govindaswamy <aarunkarthii@gmail.com>
2019-06-23 00:49:02 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,8 @@ function toggleAnswer(f) {
 			var ans = "ans"+f.id.split('ques')[1];
 			$("#"+ans).toggle();
 }
+
+function toggleSkill(f) {
+			var skill = "skill"+f.id.split('set')[1];
+			$("#"+skill).toggle();
+}
