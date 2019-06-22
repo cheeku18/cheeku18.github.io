d89d91a6e3194e1acdc8d50d6f608adbe6c1e6f7
Arun Gopal Govindaswamy <aarunkarthii@gmail.com>
2019-06-23 01:36:21 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
 function toggleAnswer(f) {
-			var ans = "ans"+f.id.split('ques')[1];
-			$("#"+ans).toggle();
+	var ans = "ans"+f.id.split('ques')[1];
+	$("#"+ans).toggle();
 }
 
 function toggleSkill(f) {
-			var skill = "skill"+f.id.split('set')[1];
-			$("#"+skill).toggle();
+	var skill = "skill"+f.id.split('set')[1];
+	$("#"+skill).toggle();
+}
+
+function loadProject(filename){
+	$("#projcontainer").show();
+	filename = filename + ".html";
+	$("#projectholder").load(filename);
+	
+}
+
+function closeProject(){
+	$("#projcontainer").hide();
 }
