f2058bb2dafbc223c0c1898401e5c532a91113fc
Arun Gopal Govindaswamy <aarunkarthii@gmail.com>
2019-06-23 22:25:22 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,28 +2,40 @@ function toggleAnswer(f) {
 	var click = f.id.split('ques')[1];
 	var ans = "ans"+click;
 	var x = document.getElementById(ans);
-	var len = document.getElementsByClassName('certification').length;
-	for(var i=1;i<=len;i++){
-		document.getElementById("ans"+i).style.display='none';
-	}
+	var change;
 	if(x.style.display=='none'){
 		x.style.display='block';
+		change = true;
+	}else{
+		change = false;
+	}
+	var len = document.getElementsByClassName('certification').length;
+	for(var i=1;i<=len;i++){
+		if(i==click && change){
+			continue;
+		}
+		document.getElementById("ans"+i).style.display='none';
 	}
 }
 
 function toggleSkill(f) {
 	var click = f.id.split('set')[1];
-	var ans = "skill"+click;
-	var x = document.getElementById(ans);
-	var len = document.getElementsByClassName('skill').length;
-	for(var i=1;i<=len;i++){
-		ans = "skill"+i;
-		document.getElementById(ans).style.display='none';
-	}
+	var skill = "skill"+click;
+	var x = document.getElementById(skill);
+	var change;
 	if(x.style.display=='none'){
 		x.style.display='block';
+		change = true;
+	}else{
+		change = false;
 	}
-
+	var len = document.getElementsByClassName('skill').length;
+	for(var i=1;i<=len;i++){
+		if(i==click && change){
+			continue;
+		}
+		document.getElementById("skill"+i).style.display='none';
+	}
 }
 
 function loadProject(filename){
